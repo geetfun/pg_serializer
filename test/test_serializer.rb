31d851d0a8dca4ae6bfbaca93f26f1b8ab6c92b9
Simon Chiu <skhchiu@gmail.com>
2010-11-21 22:00:24 -0500

    added more tests and functions

--- test/test_serializer.rb
+++ test/test_serializer.rb
@@ -17,10 +17,24 @@ module RubyPsigate
       assert_equal expectation, @result.to_xml
     end
   
-    def test_create_one_level_nested_markup_from_hash
+    def test_creates_one_level_nested_markup_from_hash
       expectation = "<Family><Dad>Bob</Dad><Mom>Jane</Mom></Family>"
       hash = {:Family => {:Dad => "Bob", :Mom => "Jane"}}
       @result = Serializer.new(hash)
+      assert_equal expectation, @result.to_xml
+    end
+    
+    def test_creates_two_level_nested_markup_from_hash
+      expectation = "<Family><Dad>Bob</Dad><Mom>Jane</Mom><Children><Son>Mike</Son><Daughter>Ann</Daughter></Children></Family>"
+      hash = { :Family => { :Dad => "Bob", :Mom => "Jane", :Children => { :Son => "Mike", :Daughter => "Ann" } } }
+      @result = Serializer.new(hash)
+      assert_equal expectation, @result.to_xml
+    end
+    
+    def test_includes_header
+      expectation = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Something>Hello World</Something>"
+      hash = { :Something => "Hello World" }
+      @result = Serializer.new(hash, :header => true)
       assert_equal expectation, @result.to_xml
     end
   
